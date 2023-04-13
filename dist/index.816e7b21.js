1f4d636b7d0f303cbcb7246d30698497d6cd1365
Alex Gerhardt <a.gerhardt04@gmail.com>
2023-04-13 09:54:59 -0500

    second commit

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("#todo-form");
+const taskType = document.querySelector("#task-type");
+const todoInput = document.querySelector("#todo-input");
+const taskOutput = document.querySelector(".task-container");
+const taskArr = [
+    {
+        todo: "wash clothes",
+        taskType: "chore"
+    }
+];
+const handleFormSubmit = (event)=>{
+    event.preventDefault();
+    const todo = {
+        todo: todoInput.value,
+        taskType: taskType.value
+    };
+    taskArr.push(todo);
+    console.log(taskArr);
+    addTaskToList();
+};
+const addTaskToList = ()=>{
+    taskArr?.forEach((task)=>{
+        taskOutput.insertAdjacentHTML("beforeend", `<div>
+      <span>${task.todo}</span>
+      <span>${task.taskType}</span>
+    </div>`);
+    });
+};
+form.addEventListener("submit", handleFormSubmit);
+
+//# sourceMappingURL=index.816e7b21.js.map
